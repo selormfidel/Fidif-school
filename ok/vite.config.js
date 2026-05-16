9f4b7bac81c70d32c2711b6cbb894d9d5a3fb943
selormfidel <selormfidel@gmail.com>
2026-05-16 03:07:45 +0000

    Set base path for Vite configuration

--- ok/vite.config.js
+++ ok/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
+  base: './',
   root: '.',
   appType: 'mpa',
   server: { host: '::', port: 8080 },
